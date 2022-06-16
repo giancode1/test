984a96fe4d189c756e2dd711ab34e9508d14b431
Giancarlo Culcay <gieg5c@gmail.com>
2022-06-15 21:56:20 -0500

    spies

--- api/src/services/book.service.test.js
+++ api/src/services/book.service.test.js
@@ -6,13 +6,20 @@ const fakeBooks = [
     name: 'Atomic Habits',
   },
 ];
+
+const mockGetAll = jest.fn(BookService, 'getBooks'); // realmente es un spy pero hay que manejar la sintaxis de jest
+
 // suplantacion de la clase
 const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+  // getAll: () => [...fakeBooks],
+  getAll: mockGetAll,
   create: () => {},
 };
 
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
 describe('Test for BookService', () => {
   let service;
@@ -25,14 +32,33 @@ describe('Test for BookService', () => {
     test('should return a list book', async () => {
       // no se conecta a la base de datos, hace mock de la base de datos
       // Arrange
-
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
-      const books = await service.getBooks();
+      const books = await service.getBooks({});
       console.log(books);
       // Assert
       expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+    });
+
+    test('should return a list book', async () => {
+      // no se conecta a la base de datos, hace mock de la base de datos
+      // Arrange
+      mockGetAll.mockResolvedValue([{
+        _id: 1,
+        name: 'Harry Potter 2',
+      }]);
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter 2');
     });
   });
 });
 // unit test, pruebas aisladas al código, lo que este relacionado con dependencias generamos mock
 // pnpm test
+
+// ahora ya tenemos un mock o estamos inyectando datafake en cada escenario de prueba
